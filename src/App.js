cdd759442ee69dc502053d9a619841db777ff998
thedorosgiatsidis <todoros_r21@hotmail.com>
2021-10-30 01:19:11 +0300

    my first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import React, { useState } from "react";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>My TODO App</h1>
+      </header>
+      <Form
+        setInputText={setInputText}
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <TodoList todos={todos} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Todo from "./Todo";
+
+function TodoList({ todos }) {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <Todo text={todo.text} key={todo.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
