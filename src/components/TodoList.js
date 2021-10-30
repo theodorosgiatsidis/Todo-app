c96487755f59ce24f3e67ed3d6a298d5fe29a510
thedorosgiatsidis <todoros_r21@hotmail.com>
2021-10-30 16:28:17 +0300

    delete Todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Todo from "./Todo";
 
-function TodoList({ todos }) {
+function TodoList({ todos, setTodos }) {
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {todos.map((todo) => (
-          <Todo text={todo.text} key={todo.id} />
+          <Todo
+            text={todo.text}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+            todo={todo}
+          />
         ))}
       </ul>
     </div>
